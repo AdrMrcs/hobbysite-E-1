134a51680a666e1188c4601908c1d55129b3738a
Bryan Francisco <franciscobryaaan@gmail.com>
2024-03-08 18:54:59 +0800

    done with minimum reqs for commissions merged commission and wiki

---
+++ commissions/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Commission(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    people_required = models.IntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'[COMMISSION] {self.title}, updated on {self.updated_on}'
+    
+    def get_absolute_url(self):
+        return reverse('commissions:commission-detail', args=[self.pk])
+    
+    class Meta:
+        ordering = ['created_on']
+
+class Comment(models.Model):
+    commission = models.ForeignKey(
+        'Commission',
+        on_delete=models.CASCADE, 
+        default=1,
+        related_name='comments',
+    )
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Comment on {self.commission.title}. PK={self.pk}'
+
+    # def get_absolute_url(self):
+    #     return reverse('commissions:commission-detail', args=[self.pk])
+
+    class Meta:
+        ordering = ['-created_on']
--- hobbysite/urls.py
+++ hobbysite/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("wiki.urls", namespace="wiki"))
+    path('', include("wiki.urls", namespace="wiki")),
+    path('', include("commissions.urls", namespace="commissions"))
 ]
