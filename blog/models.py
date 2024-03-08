292e498e46556889633dcc8f9a08e9158d0db131
AdrMrcs <adrian.villanueva@student.ateneo.edu>
2024-03-08 16:52:41 +0800

    made changes on urls.py and models.py finishing touches needed

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class ArticleCategory(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return '{}'.format(self.name)
+    
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        ArticleCategory,
+        on_delete = models.SET_NULL
+    )
+    entry = models.TextField
+
+    def __str__(self):
+        return '{}'.format(self.title)
