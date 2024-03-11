eb4ccd34a29200a283431cb49c82af9498067f6b
AdrMrcs <adrian.villanueva@student.ateneo.edu>
2024-03-11 15:52:20 +0800

    updates on models, urls, and views. finishing touches needed

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+from .models import Article, ArticleCategory
+
+
+def index(request):
+    return HttpResponse("hobby site")
+
+class BlogListView(ListView):
+    model = Article
+    template_name = 'article_list.html'
+
+class BlogDetailView(DetailView):
+    model = Article
+    template_name = 'article_detail.html'
