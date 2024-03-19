672a1012fa036c65a458bfd421a04f0b9ca66385
AdrMrcs <adrian.villanueva@student.ateneo.edu>
2024-03-19 21:28:44 +0800

    htmls finished. server functioning

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Article, ArticleCategory
+
+class Inline(admin.TabularInline):
+    model = Article
+
+class ArticleCategoryAdmin(admin.ModelAdmin):
+    model = ArticleCategory
+    inlines = [Inline,]
+
+class ArticleAdmin(admin.ModelAdmin):
+    model = Article
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(ArticleCategory, ArticleCategoryAdmin)
