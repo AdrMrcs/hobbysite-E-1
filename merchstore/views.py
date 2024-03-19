1d13b5e7de66574c0f2fcd85ad1a96639b849cbd
franch1se <ronald.francis.bautista@student.ateneo.edu>
2024-03-20 07:22:11 +0800

    removed redundant code

--- merchstore/views.py
+++ merchstore/views.py
@@ -3,22 +3,9 @@ from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
-from .models import ProductType, Product
+from .models import ProductType
 
 # Create your views here.
-
-def merch_list (request):
-    ctx = {
-        "products": ProductType.objects.all(), 
-    }
-    return render (request, 'product_list.html', ctx)
-
-def merch_detail (request, id):
-    ctx = {
-        "producttype": ProductType.objects.get(id=id), 
-        "products": Product.objects.filter(producttype__id=id), 
-    }
-    return render (request, 'product_detail.html', ctx)
 
 class ProductListView (ListView):
     model = ProductType
