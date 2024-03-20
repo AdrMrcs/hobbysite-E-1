30b21a7628e38d083b78643a86b966c744273df6
Bryan Francisco <franciscobryaaan@gmail.com>
2024-03-20 17:11:05 +0800

    merged wiki and commission

--- commissions/urls.py
+++ commissions/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import CommissionListView, CommissionDetailView
 
 urlpatterns = [
-    path('commission/list', CommissionListView.as_view(), name='commission-list'),
-    path('commission/detail/<int:pk>', CommissionDetailView.as_view(), name='commission-detail')
+    path('list', CommissionListView.as_view(), name='commission-list'),
+    path('detail/<int:pk>', CommissionDetailView.as_view(), name='commission-detail')
 ]
 
 app_name = 'commission'
