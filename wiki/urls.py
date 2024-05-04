658fee0bff2f54370de87bf8e341ec99907050f6
Bryan Francisco <franciscobryaaan@gmail.com>
2024-05-04 15:58:55 +0800

    clean wiki

--- wiki/urls.py
+++ wiki/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 
-from .views import index, WikiListView, WikiDetailView 
+from .views import WikiListView, WikiDetailView
 
 
 urlpatterns = [
-    path('', index, name="index"),
-    path('articles', WikiListView.as_view(), name="article_list"),
-    path('articles/<int:pk>', WikiDetailView.as_view(), name='article_details'),
+    path("articles", WikiListView.as_view(), name="article_list"),
+    path("articles/<int:pk>", WikiDetailView.as_view(), name="article_details"),
 ]
 
 app_name = "wiki"
