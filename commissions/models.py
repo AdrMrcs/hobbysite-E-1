39666bb9f3a7115985b3ab040096fd13c1226a6e
Bryan <franciscobryaaan@gmail.com>
2024-05-05 22:22:03 +0800

    commission: updated list view

--- commissions/models.py
+++ commissions/models.py
@@ -11,13 +11,13 @@ STATUS_CHOICES = {
         "DI" : "Discontinued",
     },
     "JOB" : {
-        1 : "Open",
-        2 : "Full",
+        "1" : "Open",
+        "2" : "Full",
     },
     "JOB_APP" : {
-        1 : "Pending",
-        2 : "Accepted",
-        3 : "Rejected",
+        "1" : "Pending",
+        "2" : "Accepted",
+        "3" : "Rejected",
     }
 }
 
@@ -28,12 +28,37 @@ class Commission(models.Model):
     # people_required = models.IntegerField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(
+        Profile,
+        null = True,
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self):
-        return f'[COMMISSION] {self.title}, updated on {self.updated_on}'
+        return self.title
     
     def get_absolute_url(self):
         return reverse('commissions:commission-detail', args=[self.pk])
+
+    @property
+    def is_open(self):
+        return self.status == "OP"
+    
+    @property
+    def is_full(self):
+        return self.status == "FU"
+    
+    @property
+    def is_completed(self):
+        return self.status == "CO"
+    
+    @property
+    def is_discontinued(self):
+        return self.status == "DI"
+    
+    @property
+    def get_status(self):
+        return STATUS_CHOICES["COMMISSION"][self.status]
     
     class Meta:
         ordering = ['created_on']
@@ -47,10 +72,14 @@ class Job(models.Model):
     )
     role = models.CharField(max_length=255)
     manpower_required = models.IntegerField()
-    status = models.CharField(max_length=25, choices=STATUS_CHOICES["JOB"], default=1)
+    status = models.CharField(max_length=25, choices=STATUS_CHOICES["JOB"], default="1")
 
     def __str__(self):
         return f'Job from {self.commission.title}. PK={self.pk}'
+    
+    @property
+    def get_status(self):
+        return STATUS_CHOICES["JOB"][self.status]
 
     class Meta:
         ordering = ['-status', '-manpower_required', 'role']
@@ -66,8 +95,12 @@ class JobApplication(models.Model):
         Profile,
         on_delete=models.CASCADE,
     )
-    status = models.CharField(max_length=25, choices=STATUS_CHOICES["JOB_APP"], default=1)
+    status = models.CharField(max_length=25, choices=STATUS_CHOICES["JOB_APP"], default="1")
     applied_on = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_status(self):
+        return STATUS_CHOICES["JOB_APP"][self.status]
 
     class Meta:
         ordering = [
--- commissions/views.py
+++ commissions/views.py
@@ -4,11 +4,19 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
 from .models import Commission, Job, JobApplication
-
+from user_management.models import Profile
 
 class CommissionListView(ListView):
     model = Commission
     template_name = 'commission_list.html'
+
+    def get_context_data(self, **kwargs):
+        user = self.request.user
+        ctx = super().get_context_data(**kwargs)
+        if user.is_authenticated:
+            ctx['user_created'] = Commission.objects.filter(created_by=user.profile)
+            # make user_applied
+        return ctx
 
 class CommissionDetailView(DetailView):
     model = Commission
