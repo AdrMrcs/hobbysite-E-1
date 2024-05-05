32e8e6c706c27c2c4533ff0d0111bd65aabdb955
AdrMrcs <adrian.villanueva@student.ateneo.edu>
2024-05-05 15:28:05 +0800

    added urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from .views import BlogDetailView, BlogListView
+from .views import BlogDetailView, BlogListView, BlogCreateView, BlogUpdateView
 
 
 urlpatterns = [
     path('articles', BlogListView.as_view(), name='article-list'),
     path('article/<int:pk>', BlogDetailView.as_view(), name='article-detail'),
+    path('article/add', BlogCreateView.as_view(), name='article-create'),
+    path('article/<int:pk>/edit', BlogUpdateView.as_view(), name='article-edit'),
 ]
 
 app_name = 'blog'
