e418df1607f13b3b1b0aad655c2a5c167be946ce
jinyuanZapi <joshua.zapico@student.ateneo.edu>
2024-05-07 02:02:50 +0800

    fixed ListView and DetailViews

--- wiki/forms.py
+++ wiki/forms.py
@@ -1,8 +1,18 @@
 from django import forms
 
-from .models import Article
+from .models import Article, Comment
 
 class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
-        fields = ['name', 'author']
+        fields = ['title', 'category', 'entry', 'headerImage']
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+class CommentForms(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['entry']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
