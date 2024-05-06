347a175b3a91b1bd0e9f9c504ef0f8b7123dcb5f
franch1se <ronald.francis.bautista@student.ateneo.edu>
2024-05-07 00:42:15 +0800

    updated models, views, urls, and added forms

--- merchstore/models.py
+++ merchstore/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from django.urls import reverse
 
+from user_management.models import Profile
 
-class ProductType(models.Model):
+# Create your models here.
+
+class ProductType (models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
 
@@ -11,20 +14,41 @@ class ProductType(models.Model):
     
     class Meta:
         ordering = ['name', ]
+        verbose_name = 'Product Type'
 
-class Product(models.Model):
+class Product (models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField(default=0)
+
+    status_choices = [
+        ('Available', 'Available'), 
+        ('On Sale', 'On Sale'), 
+        ('Out of Stock', 'Out of Stock')
+    ]
+    status = models.CharField (
+        max_length=50, 
+        choices=status_choices, 
+        default='Available', 
+    )
+
+    owner = models.ForeignKey (
+        Profile, 
+        on_delete=models.CASCADE, 
+        null=True, 
+        related_name = 'profile'
+    )
 
     producttype = models.ForeignKey (
         ProductType, 
         null = True, 
         on_delete=models.SET_NULL, 
-        related_name = 'products'
+        related_name = 'products', 
+        verbose_name = 'Product Type', 
     )
 
-    def _str__(self):
+    def __str__ (self):
         return self.name
 
     def get_absolute_url(self):
@@ -32,3 +56,30 @@ class Product(models.Model):
 
     class Meta:
         ordering = ['name', ]
+
+class Transaction (models.Model): 
+    buyer = models.ForeignKey (
+        Profile, 
+        on_delete=models.SET_NULL, 
+        null=True, 
+    )
+    product = models.ForeignKey (
+        Product, 
+        on_delete=models.SET_NULL, 
+        null=True, 
+        related_name= 'products'
+    )
+    amount = models.IntegerField(default=0)
+    status_choices = [
+        ('On Cart', 'On Cart'), 
+        ('To Pay', 'To Pay'), 
+        ('To Ship', 'To Ship'), 
+        ('To Receive', 'To Receive'), 
+        ('Delivered', 'Delivered'), 
+    ]
+    status = models.CharField (
+        max_length=50, 
+        choices=status_choices, 
+        default = 'On Cart', 
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
--- merchstore/urls.py
+++ merchstore/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
-from .views import ProductListView, ProductDetailView
-
+from .views import *
 
 urlpatterns = [
     path('items', ProductListView.as_view(), name='product-list'),
     path('item/<int:pk>', ProductDetailView.as_view(), name='product-detail'), 
+    path('item/add', ProductCreateView.as_view(), name='create-view'), 
+    path('item/<int:pk>/edit', ProductUpdateView.as_view(), name='update_view'),
+    path('cart', CartView.as_view(), name='cart-view'), 
+    path('transactions', TransactionView.as_view(), name='transaction-view')
 ]
 
 app_name = "merchstore"
