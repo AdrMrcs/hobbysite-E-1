3a288c2f7c955c02fce9fc208dd83ecc38e69513
AdrMrcs <adrian.villanueva@student.ateneo.edu>
2024-05-07 00:22:56 +0800

    comments for detailview working

--- blog/forms.py
+++ blog/forms.py
@@ -4,11 +4,10 @@ from .models import Article, Comment
 class ArticleForms(forms.ModelForm):
     class Meta:
         model = Article
-        fields = '__all__'
+        exclude = ['author']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['author'].disabled = True
 
 class CommentForms(forms.ModelForm):
     class Meta:
