50a3ff6de580e2c695a536be24ff99706e197526
Zapi <joshua.zapico@student.ateneo.edu>
2024-05-06 18:59:29 +0800

    accomplishing CreateView and UpdateView

--- wiki/views.py
+++ wiki/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.urls import reverse_lazy
 
-from .models import Article
+from .models import Article, ArticleCategory, Comment
+from .forms import ArticleForm
 
 
 class WikiListView(ListView):
-    model = Article
+    model = ArticleCategory
     template_name = 'wiki_articles.html'
 
 class WikiDetailView(DetailView):
-    model = Article
+    model = Article, Comment
     template_name = 'wiki_details.html'
     redirect_field_name = '/wiki/articles'
+
+class CreateWikiArticle(LoginRequiredMixin, CreateView):
+    model = Article
+    form_class = ArticleForm
+    template_name = 'wiki_create.html'
+
+    def get_success_url(self):
+        return reverse_lazy('ledger:articles')
