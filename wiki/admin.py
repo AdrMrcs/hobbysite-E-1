e0c9286e252cc140cffd6dc1a5858a76fb45c606
jinyuanZapi <joshua.zapico@student.ateneo.edu>
2024-05-07 02:56:59 +0800

    pep8 spacing adjustments

--- wiki/admin.py
+++ wiki/admin.py
@@ -6,15 +6,19 @@ from .models import Article, ArticleCategory, Comment
 class Inline(admin.TabularInline):
     model = Article
 
+
 class ArticleCategoryAdmin(admin.ModelAdmin):
     model = ArticleCategory
     inlines = [Inline,]
 
+
 class ArticleAdmin(admin.ModelAdmin):
     model = Article
 
+
 class CommentAdmin(admin.ModelAdmin):
     model = Comment
+
 
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(ArticleCategory, ArticleCategoryAdmin)
