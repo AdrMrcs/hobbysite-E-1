036ea681f3e98c155101b957fbfa07f448ac414f
jinyuanZapi <joshua.zapico@student.ateneo.edu>
2024-05-07 02:51:33 +0800

    finished Create and Edit functionality

--- wiki/urls.py
+++ wiki/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from .views import WikiListView, WikiDetailView
+from .views import WikiListView, WikiDetailView, CreateWikiArticle, EditWikiArticle
 
 
 urlpatterns = [
     path("articles", WikiListView.as_view(), name="article_list"),
     path("articles/<int:pk>", WikiDetailView.as_view(), name="article_details"),
+    path('article/add', CreateWikiArticle.as_view(), name='article_create'),
+    path('article/<int:pk>/edit', EditWikiArticle.as_view(), name='article_edit'),
 ]
 
 app_name = "wiki"
